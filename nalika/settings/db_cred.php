3ea9984f1404ab20ecee73fcfb1e3a7b7d580d90
eliz-kyera <87820310+eliz-kyera@users.noreply.github.com>
2022-12-06 18:50:27 +0000

    push

--- nalika/settings/db_cred.php
+++ nalika/settings/db_cred.php
@@ -17,7 +17,7 @@
 //Database credentials
 
 
-define("DATABASE", "heroku_6ad55d9ffb3eba4");
+define("DATABASE", "heroku_cefdfa3a65d7cb7");
 define("SERVER", "us-cdbr-east-06.cleardb.net");
 define("USERNAME", "b9c65028092515");
 define("PASSWD", "9bbf3880");
