ea62fb72ab2fe7d0e5de815a9cb4efcd4748a179
eliz-kyera <87820310+eliz-kyera@users.noreply.github.com>
2022-12-07 19:40:37 +0000

    new

--- nalika/settings/db_cred.php
+++ nalika/settings/db_cred.php
@@ -2,10 +2,10 @@
 
 
 
-define("DATABASE", "nissie");
-define("SERVER", "localhost");
-define("USERNAME", "root");
-define("PASSWD", "");
+// define("DATABASE", "nissie");
+// define("SERVER", "localhost");
+// define("USERNAME", "root");
+// define("PASSWD", "");
 ?>
 
 
@@ -14,13 +14,13 @@ define("PASSWD", "");
 
 
 <?php
-//Database credentials
+// Database credentials
 
 
-// define("DATABASE", "heroku_cefdfa3a65d7cb7");
-// define("SERVER", "us-cdbr-east-06.cleardb.net");
-// define("USERNAME", "bb75b7e09c907c");
-// define("PASSWD", "6f1fcad4");
+define("DATABASE", "heroku_cefdfa3a65d7cb7");
+define("SERVER", "us-cdbr-east-06.cleardb.net");
+define("USERNAME", "bb75b7e09c907c");
+define("PASSWD", "6f1fcad4");
 
 ?>
 
